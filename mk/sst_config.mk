2b3a529a9c8131bcd7258f2a7f30f2d3ed812a04
Max Ruttenberg <mrutt@cs.washington.edu>
2023-09-19 10:20:50 -0700

    save exporting custom std mem

--- mk/sst_config.mk
+++ mk/sst_config.mk
@@ -1,6 +1,6 @@
 DRV_DIR ?= $(shell git rev-parse --show-toplevel)
 # CHANGEME - set the path to your sst-elements install
-SST_ELEMENTS_INSTALL_DIR=/install
+SST_ELEMENTS_INSTALL_DIR=$(HOME)/work/AGILE/sst-elements/install
 SST_ELEMENTS_CXXFLAGS += -I$(SST_ELEMENTS_INSTALL_DIR)/include
 SST_ELEMENTS_LDFLAGS  += -L$(SST_ELEMENTS_INSTALL_DIR)/lib -Wl,-rpath,$(SST_ELEMENTS_INSTALL_DIR)/lib
 SST_ELEMENTS_LDFLAGS  += -L$(SST_ELEMENTS_INSTALL_DIR)/lib/sst-elements-library
