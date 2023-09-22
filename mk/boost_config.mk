8cd2dda117945cc3def1619c8730d71878bb1b01
Max Ruttenberg <mrutt@cs.washington.edu>
2023-09-22 13:28:18 -0400

    docker file update

--- mk/boost_config.mk
+++ mk/boost_config.mk
@@ -1,5 +1,5 @@
 # CHANGME - set the path to your boost installation
-BOOST_INSTALL_DIR=$(HOME)/package-install/boost_1_82_0
+BOOST_INSTALL_DIR=/install
 BOOST_CXXFLAGS += -I$(BOOST_INSTALL_DIR)/include
 BOOST_LDFLAGS  += -L$(BOOST_INSTALL_DIR)/lib -Wl,-rpath=$(BOOST_INSTALL_DIR)/lib
 
