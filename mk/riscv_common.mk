e2f57c6c1585cb58100e8fa9b7f14396e1cada02
Max Ruttenberg <mrutt@cs.washington.edu>
2023-10-05 15:39:24 -0700

    [drvr] adds libs makefile var

--- mk/riscv_common.mk
+++ mk/riscv_common.mk
@@ -15,9 +15,10 @@ vpath %.cpp $(DRV_DIR)/riscv-examples/platform_$(PLATFORM)
 ARCH:=rv64imafd
 ABI:=lp64d
 
-CXXFLAGS := -O2  -march=$(ARCH) -mabi=$(ABI)
-CFLAGS   := -O2  -march=$(ARCH) -mabi=$(ABI)
-LDFLAGS  :=
+CXXFLAGS += -O2  -march=$(ARCH) -mabi=$(ABI)
+CFLAGS   += -O2  -march=$(ARCH) -mabi=$(ABI)
+LDFLAGS  +=
+LIBS += 
 
 -include $(DRV_DIR)/riscv-examples/platform_$(PLATFORM)/common.mk
 
@@ -39,7 +40,7 @@ $(CXXOBJECT): %.o: %.cpp
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
 $(TARGET): %.riscv: $(COBJECT) $(CXXOBJECT) $(ASMOBJECT)
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $@ $(filter %.o,$^)
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $@ $(filter %.o,$^) $(LIBS)
 
 .PHONY: clean
 clean:
