28e736f8041dd5df2a7ebb1f9b30854265c6b1c7
Max Ruttenberg <mrutt@cs.washington.edu>
2023-10-17 17:25:53 -0700

    [mk] guard around boost

--- mk/boost_config.mk
+++ mk/boost_config.mk
@@ -1,5 +1,8 @@
 # CHANGME - set the path to your boost installation
-BOOST_INSTALL_DIR=/install
+ifndef _BOOST_CONFIG_MK_
+_BOOST_CONFIG_MK_ = 1
+BOOST_INSTALL_DIR=$(HOME)/package-install/boost_1_82_0
 BOOST_CXXFLAGS += -I$(BOOST_INSTALL_DIR)/include
 BOOST_LDFLAGS  += -L$(BOOST_INSTALL_DIR)/lib -Wl,-rpath=$(BOOST_INSTALL_DIR)/lib
 
+endif
