5cc3172c213ca71c547dd665e3fd36643d5766e1
Max Ruttenberg <mrutt@cs.washington.edu>
2023-10-17 17:37:03 -0700

    [mk] fix paths

--- mk/boost_config.mk
+++ mk/boost_config.mk
@@ -1,7 +1,7 @@
 # CHANGME - set the path to your boost installation
 ifndef _BOOST_CONFIG_MK_
 _BOOST_CONFIG_MK_ = 1
-BOOST_INSTALL_DIR=$(HOME)/package-install/boost_1_82_0
+BOOST_INSTALL_DIR=/install
 BOOST_CXXFLAGS += -I$(BOOST_INSTALL_DIR)/include
 BOOST_LDFLAGS  += -L$(BOOST_INSTALL_DIR)/lib -Wl,-rpath=$(BOOST_INSTALL_DIR)/lib
 
--- mk/install_config.mk
+++ mk/install_config.mk
@@ -4,7 +4,7 @@ _INSTALL_CONFIG_MK_ := 1
 DRV_DIR ?= $(shell git rev-parse --show-toplevel)
 # CHANGEME - where do you want to install the libraries and
 # header files for drv
-DRV_INSTALL_DIR = $(DRV_DIR)/install
+DRV_INSTALL_DIR = /install
 
 DRV_BIN_DIR = $(DRV_INSTALL_DIR)/bin
 DRV_INCLUDE_DIR = $(DRV_INSTALL_DIR)/include
