3645853c1aca653eacb8c4cd0e43e7d995435cb2
Max Ruttenberg <mrutt@cs.washington.edu>
2023-10-20 12:50:50 -0700

    [drvr] poke a memory address test

---
+++ riscv-examples/poke/main.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <string.h>
+#include <pandohammer/mmio.h>
+
+inline void store_byte(volatile char *addr, char val)
+{
+    asm volatile("sb %0, 0(%1)" : : "r"(val), "r"(addr));
+}
+
+int main()
+{
+    char *store_addr = (char *)STORE_ADDR;
+    ph_print_hex((unsigned long)store_addr);
+    store_byte(store_addr, STORE_VALUE);
+    return 0;
+}
