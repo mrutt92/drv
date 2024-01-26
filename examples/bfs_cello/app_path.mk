6daa0c5b2cc660380c557224a845da7446ff9325
Max Ruttenberg <mrutt@cs.washington.edu>
2024-01-25 20:07:28 -0800

    forward bfs working with cello

---
+++ examples/bfs_cello/app_path.mk
@@ -0,0 +1,2 @@
+APP_NAME = bfs_cello
+APP_PATH = $(DRV_DIR)/examples/bfs_cello
---
+++ examples/bfs_cello/tests.mk
@@ -0,0 +1,13 @@
+####################
+# CHANGE ME: TESTS #
+####################
+# TESTS += $(call test-name,[threads],[cores],[pods],[pxns],[graph],[start])
+TESTS += $(call test-name,1,1,1,1,u10k16,0)
+# TESTS += $(call test-name,2,1,1,1,u10k16,0)
+# TESTS += $(call test-name,4,1,1,1,u10k16,0)
+# TESTS += $(call test-name,8,1,1,1,u10k16,0)
+TESTS += $(call test-name,16,1,1,1,u10k16,0)
+TESTS += $(call test-name,16,8,1,1,u10k16,0)
+
+
+
