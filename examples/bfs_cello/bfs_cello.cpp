3c4ecdc1b415b51f5a5ba7784f5dbad1234744bc
Max Ruttenberg <mrutt@cs.washington.edu>
2024-01-30 17:10:21 -0800

    save

--- examples/bfs_cello/bfs_cello.cpp
+++ examples/bfs_cello/bfs_cello.cpp
@@ -227,7 +227,7 @@ int CelloMain(int argc, char* argv[]) {
     double csr_end_time = DrvAPI::seconds();
 
     printf("CSR CONSTRUCTION TIME: %2.9lf s\n", csr_end_time - csr_start_time);    
-    DrvAPI::outputStatistics("csr_construction");
+    DrvAPI::outputStatistics("breadth_first_search_start");
 
     double bfs_start_time = DrvAPI::seconds();
     // phase 2. run bfs
@@ -312,7 +312,7 @@ int CelloMain(int argc, char* argv[]) {
 
     double bfs_end_time = DrvAPI::seconds();
     printf("BFS TIME: %2.9lf s\n", bfs_end_time - bfs_start_time);
-    DrvAPI::outputStatistics("breadth_first_search");
+    DrvAPI::outputStatistics("breadth_first_search_end");
 
     cello::parallel_for(0, v, 1, [=] (int32_t i) {
         if (distance[i] != ref_distance[i]) {
