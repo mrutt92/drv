c27641494db85e014ab237c42f35df123a8fbc02
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-07 14:20:28 -0800

    pagerank

---
+++ examples/pr_cello/tests.mk
@@ -0,0 +1,8 @@
+####################
+# CHANGE ME: TESTS #
+####################
+# TESTS += $(call test-name,[threads],[cores],[pods],[pxns],[graph])
+TESTS += $(call test-name,1,1,1,1,u10k16)
+TESTS += $(call test-name,16,8,1,1,u10k16)
+TESTS += $(call test-name,16,8,1,1,u16k16)
+
