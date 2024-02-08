d3d853257e587b789d6ddc1ec5a0adaac99bd74c
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-08 12:41:49 -0800

    [cello] bfs bugfix

--- examples/cello_bfs/tests.mk
+++ examples/cello_bfs/tests.mk
@@ -2,33 +2,8 @@
 # CHANGE ME: TESTS #
 ####################
 # TESTS += $(call test-name,[threads],[cores],[pods],[pxns],[graph],[start])
-# TESTS += $(call test-name,1,1,1,1,u10k16,0)
-# TESTS += $(call test-name,2,1,1,1,u10k16,0)
-# TESTS += $(call test-name,4,1,1,1,u10k16,0)
-# TESTS += $(call test-name,8,1,1,1,u10k16,0)
-# TESTS += $(call test-name,16,1,1,1,u10k16,0)
-# TESTS += $(call test-name,16,8,1,1,u10k16,0)
 
-# these segfault
 TESTS += $(call test-name,16,8,1,1,u12k16,0)
-TESTS += $(call test-name,16,32,1,1,u12k16,0)
-# TESTS += $(call test-name,16,32,1,1,u12k16,0)
-
-#TESTS += $(call test-name,8,64,1,1,u12k16,0)
-#TESTS += $(call test-name,8,64,2,1,u12k16,0)
-
-# TESTS += $(call test-name,16,16,1,1,u12k16,0)
-
-# TESTS += $(call test-name,1,1,1,1,u12k16,0)
-# TESTS += $(call test-name,2,1,1,1,u12k16,0)
-# TESTS += $(call test-name,4,1,1,1,u12k16,0)
-# TESTS += $(call test-name,8,1,1,1,u12k16,0)
-# TESTS += $(call test-name,16,1,1,1,u12k16,0)
 
 TESTS += $(call test-name,16,8,1,1,u16k16,0)
-# TESTS += $(call test-name,16,16,1,1,u16k16,0)
-# TESTS += $(call test-name,16,16,1,1,g10k16,0)
-# TESTS += $(call test-name,16,16,1,1,g16k16,0)
-
-
-
+TESTS += $(call test-name,16,8,1,1,g16k16,0)
