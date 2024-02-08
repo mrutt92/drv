ba8a6f782a5acec6ce182a8949b144d138d91796
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-08 10:43:37 -0800

    consolidate copy-and-pasted code

--- examples/cello_bfs/template.mk
+++ examples/cello_bfs/template.mk
@@ -10,10 +10,12 @@ include $(DRV_DIR)/mk/cello.mk
 
 vpath %.c   $(APP_PATH)
 vpath %.cpp $(APP_PATH)
-vpath %.c   $(APP_PATH)/sparse_matrix_helpers
-vpath %.cpp $(APP_PATH)/sparse_matrix_helpers
+vpath %.c   $(DRV_DIR)/common/sparse_matrix_helpers
+vpath %.cpp $(DRV_DIR)/common/sparse_matrix_helpers
 
 APP_EXE ?= $(APP_PATH)/$(test-name)/$(APP_NAME).so
+
+INPUT_DIR := $(DRV_DIR)/inputs/sparse-inputs
 
 SIM_OPTIONS += --core-threads=$(threads) --pod-cores=$(cores)
 SIM_OPTIONS += --pxn-pods=$(pods) --num-pxn=$(pxns)
@@ -21,13 +23,13 @@ SIM_OPTIONS += --drvx-stack-in-l1sp
 SIM_OPTIONS += --core-stats --stats-load-level=3
 TAG_BREAKDOWN_OPTIONS += --start-tag=breadth_first_search_start
 TAG_BREAKDOWN_OPTIONS += --end-tag=breadth_first_search_end
-SIM_ARGS += $(APP_PATH)/sparse-inputs/$(graph).mtx $(start)
+SIM_ARGS += $(INPUT_DIR)/$(graph).mtx $(start)
 SIM_THREADS := 1
 include $(DRV_DIR)/mk/config.mk
 include $(DRV_DIR)/mk/application_common.mk
 
 CXXFLAGS += $(CELLO_CXXFLAGS)
-CXXFLAGS += -I$(APP_PATH)/sparse_matrix_helpers
+CXXFLAGS += -I$(DRV_DIR)/common/sparse_matrix_helpers
 CELLO_OBJECTS := $(CELLO_CXXSOURCES_DRVX:.cpp=.o)
 
 $(APP_NAME).so: breadth_first_search_graph.o
@@ -36,7 +38,7 @@ $(APP_NAME).so: read_graph.o
 $(APP_NAME).so: transpose_graph.o
 $(APP_NAME).so: $(CELLO_OBJECTS)
 
-run: $(APP_PATH)/sparse-inputs/$(graph).mtx
+run: $(INPUT_DIR)/$(graph).mtx
 
-$(APP_PATH)/sparse-inputs/$(graph).mtx:
+$(INPUT_DIR)/$(graph).mtx:
 	$(MAKE) -C $(dir $@)  $(graph).mtx
