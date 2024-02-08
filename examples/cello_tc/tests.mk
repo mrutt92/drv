b2c89457b822bbe6220a7fc505699c0199087bf4
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-07 22:20:13 -0800

    tc opts

--- examples/cello_tc/tests.mk
+++ examples/cello_tc/tests.mk
@@ -4,5 +4,6 @@
 # TESTS += $(call test-name,[threads],[cores],[pods],[pxns],[graph])
 #TESTS += $(call test-name,1,1,1,1,u10k16)
 TESTS += $(call test-name,16,8,1,1,u10k16)
+TESTS += $(call test-name,16,8,1,1,g10k16)
 TESTS += $(call test-name,16,8,1,1,ca-GrQc)
 
