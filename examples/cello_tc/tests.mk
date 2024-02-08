e822b104742c27c64f9bc1a7b1b158a3dda85653
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-07 21:51:49 -0800

    asserts that the input graph is undirected

--- examples/cello_tc/tests.mk
+++ examples/cello_tc/tests.mk
@@ -3,11 +3,6 @@
 ####################
 # TESTS += $(call test-name,[threads],[cores],[pods],[pxns],[graph])
 #TESTS += $(call test-name,1,1,1,1,u10k16)
-#TESTS += $(call test-name,16,8,1,1,u10k16)
-#TESTS += $(call test-name,16,8,1,1,mycielskian4)
-#TESTS += $(call test-name,16,8,1,1,mycielskian9)
-#TESTS += $(call test-name,16,8,1,1,email-Eu-core)
+TESTS += $(call test-name,16,8,1,1,u10k16)
 TESTS += $(call test-name,16,8,1,1,ca-GrQc)
-#TESTS += $(call test-name,16,8,1,1,p2p-Gnutella08) # 2383 triangles
-#TESTS += $(call test-name,16,8,1,1,email-Enron) # 727044 triangles
 
