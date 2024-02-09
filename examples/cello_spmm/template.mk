1adf4c28a904cee2f1b677326a7fdd493959c939
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-09 12:28:37 -0800

    [cello] spmm debugged upto output csr generation

--- examples/cello_spmm/template.mk
+++ examples/cello_spmm/template.mk
@@ -7,11 +7,14 @@ include app_path.mk
 
 DRV_DIR := $(shell git rev-parse --show-toplevel)
 include $(DRV_DIR)/mk/cello.mk
+include $(DRV_DIR)/mk/eigen_config.mk
 
 vpath %.c   $(APP_PATH)
 vpath %.cpp $(APP_PATH)
 vpath %.c   $(DRV_DIR)/common/sparse_matrix_helpers
 vpath %.cpp $(DRV_DIR)/common/sparse_matrix_helpers
+vpath %.c   $(DRV_DIR)/common/util
+vpath %.cpp $(DRV_DIR)/common/util
 
 APP_EXE ?= $(APP_PATH)/$(test-name)/$(APP_NAME).so
 
@@ -23,14 +26,16 @@ SIM_OPTIONS += --drvx-stack-in-l1sp
 SIM_OPTIONS += --core-stats --stats-load-level=3
 TAG_BREAKDOWN_OPTIONS += --start-tag=breadth_first_search_start
 TAG_BREAKDOWN_OPTIONS += --end-tag=breadth_first_search_end
-SIM_ARGS += $(APP_PATH)/sparse-inputs/$(m0).mtx
-SIM_ARGS += $(APP_PATH)/sparse-inputs/$(m1).mtx
+SIM_ARGS += $(INPUT_DIR)/$(m0).mtx
+SIM_ARGS += $(INPUT_DIR)/$(m1).mtx
 SIM_THREADS := 1
 include $(DRV_DIR)/mk/config.mk
 include $(DRV_DIR)/mk/application_common.mk
 
 CXXFLAGS += $(CELLO_CXXFLAGS)
+CXXFLAGS += $(EIGEN_CXXFLAGS)
 CXXFLAGS += -I$(DRV_DIR)/common/sparse_matrix_helpers
+CXXFLAGS += -I$(DRV_DIR)/common/
 CELLO_OBJECTS := $(CELLO_CXXSOURCES_DRVX:.cpp=.o)
 
 $(APP_NAME).so: mmio.o
