2ddd5d4f0dd1d724260a4dd0483c33e013f55c51
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-09 16:13:23 -0800

    [cello] add a heart beat to spmm

--- examples/cello_spmm/tests.mk
+++ examples/cello_spmm/tests.mk
@@ -4,5 +4,7 @@
 # TESTS += $(call test-name,[threads],[cores],[pods],[pxns],[m0],[m1])
 #TESTS += $(call test-name,1,1,1,1,u10k16,g10k16)
 TESTS += $(call test-name,1,1,1,1,u7k1,g7k1)
+TESTS += $(call test-name,16,8,1,1,u7k1,g7k1)
+TESTS += $(call test-name,16,8,1,1,u10k16,g10k16)
 
 
