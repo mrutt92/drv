cff695ff5a7c2de59ea5e842a348c43f742e6b86
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-13 13:42:17 -0800

    guard dramsim3 and ramulator with 'directory exists' check

--- mk/dramsim3_config.mk
+++ mk/dramsim3_config.mk
@@ -3,8 +3,8 @@
 
 ifndef _DRAMSIM3_CONFIG_MK_
 _DRAMSIM3_CONFIG_MK_ := 1
-DRAMSIM3 := yes
 DRAMSIM3_DIR := /root/sst-dramsim3-src
+DRAMSIM3 := $(shell [ -d $(DRAMSIM3_DIR) ] && echo yes || echo no)
 DRAMSIM3_TEST_CONFIG ?= $(DRAMSIM3_DIR)/configs/HBM2_4Gb_x128.ini
 DRAMSIM3_CXXFLAGS += -isystem $(DRAMSIM3_DIR)/src
 DRAMSIM3_LDFLAGS  +=
