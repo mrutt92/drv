ed358204b998166a4e090f3738a58f0a6540b9bd
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-13 10:33:52 -0800

    [cello] gups timer

--- examples/cello_gups/cello_gups.cpp
+++ examples/cello_gups/cello_gups.cpp
@@ -2,8 +2,10 @@
 // Copyright (c) 2024 University of Washington
 #include <DrvAPI.hpp>
 #include <cello.hpp>
+#include <util/timer.hpp>
 
 using namespace DrvAPI;
+using namespace util;
 
 template <typename T>
 using pointer = DrvAPIPointer<T>;
@@ -14,11 +16,18 @@ int CelloMain(int argc, char *argv[])
     gups_table_size = std::stoll(argv[1]);
     updates = std::stoll(argv[2]);
     printf("GUPS: table-size = %" PRId64 ", updates = %" PRId64 "\n", gups_table_size, updates);
-    pointer<int64_t> table = DrvAPI::DrvAPIMemoryAlloc(DrvAPIMemoryDRAM, gups_table_size * sizeof(int64_t));
-    cello::parallel_for(0l, updates, 1l, [table, gups_table_size](int64_t i) {
-        int64_t index = rand() % gups_table_size;
-        int64_t value = table[index];
-        table[index] = value ^ i;
-    });
+    pointer<int64_t> table=0;
+    {
+        timer _("init");
+        table = DrvAPI::DrvAPIMemoryAlloc(DrvAPIMemoryDRAM, gups_table_size * sizeof(int64_t));
+    }
+    {
+        timer _("gups");
+        cello::parallel_for(0l, updates, 1l, [table, gups_table_size](int64_t i) {
+            int64_t index = rand() % gups_table_size;
+            int64_t value = table[index];
+            table[index] = value ^ i;
+        });
+    }
     return 0;
 }
