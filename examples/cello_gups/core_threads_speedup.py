ea64eee69ef5e7ba407408a8ccb17b663b4969b7
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-13 08:39:33 -0800

    [cello] gups benchmark

---
+++ examples/cello_gups/core_threads_speedup.py
@@ -0,0 +1,6 @@
+def output(threads, cores, table_size, updates):
+    return "TESTS += $(call test-name,{},{},1,1,{},{})".format(threads, cores, table_size, updates)
+
+for cores in [1, 2, 4, 8]:
+    for threads in [1, 2, 3, 4]:
+        print(output(threads, cores, (2**30)/8, 10**3))
