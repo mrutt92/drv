304641501fdb2e6dee359b849fdbcb4bfc243bb9
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-13 09:16:14 -0800

    [cello] gups fixup

--- examples/cello_gups/core_threads_speedup.py
+++ examples/cello_gups/core_threads_speedup.py
@@ -3,4 +3,4 @@ def output(threads, cores, table_size, updates):
 
 for cores in [1, 2, 4, 8]:
     for threads in [1, 2, 3, 4]:
-        print(output(threads, cores, (2**30)/8, 10**3))
+        print(output(threads, cores, (2**30)//8, 10**3))
