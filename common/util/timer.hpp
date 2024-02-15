226498d55ea10606b0158a3a103e35215bf7416f
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-15 12:21:00 -0800

    [cello] timer calls outputStatistics()

--- common/util/timer.hpp
+++ common/util/timer.hpp
@@ -6,11 +6,13 @@ namespace util
 struct timer {
     timer(const std::string &name) : name(name) {
         start = DrvAPI::seconds();
+        DrvAPI::outputStatistics(name+"_start");
     }
 
     ~timer() {
         stop = DrvAPI::seconds();
         printf("%20s: Elapsed time: %2.9lf seconds\n", name.c_str(), stop - start);
+        DrvAPI::outputStatistics(name+"_stop");
     }
 
     std::string name;
