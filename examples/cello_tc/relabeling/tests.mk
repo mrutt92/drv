b1d7f124cf1604950d550ef44578fb1989b03af6
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-16 13:07:09 -0800

    [cello] tc heartbeat

--- examples/cello_tc/relabeling/tests.mk
+++ examples/cello_tc/relabeling/tests.mk
@@ -2,9 +2,17 @@
 # CHANGE ME: TESTS #
 ####################
 # TESTS += $(call test-name,[threads],[cores],[pods],[pxns],[graph],[relabel])
-TESTS += $(call test-name,1,1,1,1,u10k16,yes)
-TESTS += $(call test-name,1,1,1,1,u10k16,no)
-TESTS += $(call test-name,1,1,1,1,g10k16,yes)
-TESTS += $(call test-name,1,1,1,1,g10k16,no)
+# TESTS += $(call test-name,1,1,1,1,u10k16,yes)
+# TESTS += $(call test-name,1,1,1,1,u10k16,no)
+# TESTS += $(call test-name,1,1,1,1,g10k16,yes)
+#TESTS += $(call test-name,1,1,1,1,g10k16,no)
+#TESTS += $(call test-name,16,8,1,1,g16k16,yes)
+#TESTS += $(call test-name,8,8,1,1,g16k16,no)
+#TESTS += $(call test-name,8,8,1,1,g11k16,yes)
+#TESTS += $(call test-name,8,8,1,1,g11k16,no)
+#TESTS += $(call test-name,8,8,1,1,g12k16,yes)
+#TESTS += $(call test-name,8,8,1,1,g13k16,yes)
+TESTS += $(call test-name,8,8,1,1,g10k16,yes)
+
 
 
