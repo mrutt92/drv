2157df486c82dcc08a17432bdc389840a6e027f8
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-21 15:43:47 -0600

    [cello_jaccard] fixups

--- examples/cello_jaccard/tests.mk
+++ examples/cello_jaccard/tests.mk
@@ -2,5 +2,5 @@
 # CHANGE ME: TESTS #
 ####################
 # TESTS += $(call test-name,[pxns],[pods],[cores],[threads],[graph])
-TESTS += $(call test-name,1,1,1,1,u10k16)
+TESTS += $(call test-name,1,1,1,1,u7k1)
 
