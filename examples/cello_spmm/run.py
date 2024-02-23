26e67d18f9e5273516b28fc0fc9dac0f349aaebd
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-23 15:00:03 -0800

    testbenches setup

--- examples/cello_spmm/run.py
+++ examples/cello_spmm/run.py
@@ -3,9 +3,9 @@ import itertools
 import testbench as tb
 
 class SPMMTestbench(tb.Testbench):
-    CORES = [1, 2]
-    THREADS = [1, 2]
-    INPUTS = [('u7k1','g7k1')]
+    CORES = [1, 2, 3, 4, 5, 6, 7, 8]
+    THREADS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    INPUTS = [('u10k4','g10k4')]
 
     def __init__(self, tbname):
         super().__init__(tbname)
