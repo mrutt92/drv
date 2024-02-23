d03e6064ad7308fd510a9644b19d0528aa6281ae
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-23 15:00:03 -0800

    testbenches setup

--- examples/cello_bfs/run.py
+++ examples/cello_bfs/run.py
@@ -3,9 +3,9 @@ import itertools
 import testbench as tb
 
 class BFSTestbench(tb.Testbench):
-    CORES   = [1,2]
-    THREADS = [1,2]
-    INPUTS  = [('u7k1','0')]
+    CORES = [1, 2, 3, 4, 5, 6, 7, 8]
+    THREADS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    INPUTS  = [('u16k16','0')]
     def __init__(self, tbname):
         super().__init__(tbname)
 
--- examples/cello_pr/run.py
+++ examples/cello_pr/run.py
@@ -3,8 +3,8 @@ import re
 import testbench as tb
 
 class PRTestbench(tb.Testbench):
-    CORES = [1, 2]
-    THREADS = [1, 2]
+    CORES = [1, 2, 3, 4, 5, 6, 7, 8]
+    THREADS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     INPUTS = ['u7k1']
     
     def __init__(self, tbname):
