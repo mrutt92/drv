5d5daa9de94250bb7b686bb7a78f882ae640f441
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-27 10:13:26 -0800

    sweep with simple memory system (latency=70ns)

--- examples/cello_spmm/run.py
+++ examples/cello_spmm/run.py
@@ -3,7 +3,7 @@ import itertools
 import testbench as tb
 
 class SPMMTestbench(tb.Testbench):
-    CORES = [1, 2, 3, 4, 5, 6, 7, 8]
+    CORES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     THREADS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     INPUTS = [('u10k4','g10k4')]
 
