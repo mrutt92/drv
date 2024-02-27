4bcc4946bfc28d71a3e5084f6f6414d0fcd59c8f
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-27 10:13:26 -0800

    sweep with simple memory system (latency=70ns)

--- examples/cello_gups/run.py
+++ examples/cello_gups/run.py
@@ -3,7 +3,7 @@ import itertools
 import testbench as tb
 
 class GUPSTestbench(tb.Testbench):
-    CORES = [1, 2, 3, 4, 5, 6, 7, 8]
+    CORES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     THREADS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     INPUTS = [((2**29)/8, 1e6)]
     
--- examples/cello_tc/run.py
+++ examples/cello_tc/run.py
@@ -3,7 +3,7 @@ import itertools
 import testbench as tb
 
 class TCTestbench(tb.Testbench):
-    CORES = [1, 2, 3, 4, 5, 6, 7, 8]
+    CORES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     THREADS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     INPUTS = ['u12k16']
 
