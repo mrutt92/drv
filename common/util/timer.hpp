135ee22155b86a8b13ba4a4a0d30e0d12e2d0027
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-28 21:14:00 -0800

    [cello] timer tracks flops

--- common/util/timer.hpp
+++ common/util/timer.hpp
@@ -6,16 +6,33 @@ namespace util
 struct timer {
     timer(const std::string &name) : name(name) {
         start = DrvAPI::seconds();
+        flops_start = DrvAPI::float_type::Stats();
+        double_start = DrvAPI::double_type::Stats();
         DrvAPI::outputStatistics(name+"_start");
     }
 
     ~timer() {
         stop = DrvAPI::seconds();
         printf("%20s: Elapsed time: %2.9lf seconds\n", name.c_str(), stop - start);
+        flops_stop = DrvAPI::float_type::Stats();
+        double_stop = DrvAPI::double_type::Stats();
+        printf("%20s: fadd: %d\n", name.c_str(),
+               (flops_stop.num_add+double_stop.num_add)-(flops_start.num_add+double_start.num_add));
+        printf("%20s: fsub: %d\n", name.c_str(),
+                (flops_stop.num_sub+double_stop.num_sub)-(flops_start.num_sub+double_start.num_sub));
+        printf("%20s: fmul: %d\n", name.c_str(),
+                (flops_stop.num_mul+double_stop.num_mul)-(flops_start.num_mul+double_start.num_mul));
+        printf("%20s: fdiv: %d\n", name.c_str(),
+                (flops_stop.num_div+double_stop.num_div)-(flops_start.num_div+double_start.num_div));
+        printf("%20s: fmadd: %d\n", name.c_str(),
+                (flops_stop.num_muladd+double_stop.num_muladd)-(flops_start.num_muladd+double_start.num_muladd));
         fflush(stdout);
         DrvAPI::outputStatistics(name+"_stop");
     }
-
+    DrvAPI::numeric_stats flops_start;
+    DrvAPI::numeric_stats flops_stop;
+    DrvAPI::numeric_stats double_start;
+    DrvAPI::numeric_stats double_stop;
     std::string name;
     double start;
     double stop;
