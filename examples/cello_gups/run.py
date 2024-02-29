0e7c81813311861431d0dee876623110f4c526b8
Max Ruttenberg <mrutt@cs.washington.edu>
2024-02-29 09:50:10 -0800

    fix annoying thing in gups run.py

--- examples/cello_gups/run.py
+++ examples/cello_gups/run.py
@@ -5,7 +5,7 @@ import testbench as tb
 class GUPSTestbench(tb.Testbench):
     CORES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     THREADS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-    INPUTS = [((2**29)/8, 1e6)]
+    INPUTS = [ (int(x),int(y)) for (x,y) in [((2**29)/8, 1e6)] ]
     
     def __init__(self, tbname):
         super().__init__(tbname)
