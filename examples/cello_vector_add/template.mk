ac3b93e323453f4b7269bedd63ee551ad801a9e7
Max Ruttenberg <mrutt@cs.washington.edu>
2024-03-08 09:18:01 -0800

    cello_vector_add

---
+++ examples/cello_vector_add/template.mk
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2024 University of Washington
+
+# import parameters and APP_PATH
+include parameters.mk
+include app_path.mk
+
+DRV_DIR := $(shell git rev-parse --show-toplevel)
+memsys := Latency70ns-1Greq
+include $(DRV_DIR)/mk/cello.mk
+
+vpath %.c   $(APP_PATH)
+vpath %.cpp $(APP_PATH)
+vpath %.c   $(DRV_DIR)/common/util
+vpath %.cpp $(DRV_DIR)/common/util
+
+APP_EXE ?= $(APP_PATH)/$(test-name)/$(APP_NAME).so
+
+INPUT_DIR := $(DRV_DIR)/inputs/sparse-inputs
+
+SIM_OPTIONS += --core-threads=$(threads) --pod-cores=$(cores)
+SIM_OPTIONS += --pxn-pods=$(pods) --num-pxn=$(pxns)
+SIM_OPTIONS += --core-stats --stats-load-level=3
+SIM_ARGS += $(n)
+SIM_THREADS := 1
+include $(DRV_DIR)/mk/config.mk
+include $(DRV_DIR)/mk/application_common.mk
+
+CXXFLAGS += $(CELLO_CXXFLAGS)
+CXXFLAGS += -I$(DRV_DIR)/common/
+CELLO_OBJECTS := $(CELLO_CXXSOURCES_DRVX:.cpp=.o)
+
+$(APP_NAME).so: $(CELLO_OBJECTS)
