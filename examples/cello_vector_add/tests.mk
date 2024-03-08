231dd8c590d9f2f270c5551da0908b491fff6018
Max Ruttenberg <mrutt@cs.washington.edu>
2024-03-08 09:18:01 -0800

    cello_vector_add

---
+++ examples/cello_vector_add/tests.mk
@@ -0,0 +1,10 @@
+####################
+# CHANGE ME: TESTS #
+####################
+# TESTS += $(call test-name,[threads],[cores],[pods],[pxns],[n])
+#TESTS += $(call test-name,1,1,1,1,1024)
+#TESTS += $(call test-name,1,1,1,1,65536)
+#TESTS += $(call test-name,1,1,1,1,262144)
+#TESTS += $(call test-name,1,1,1,1,524288)
+TESTS += $(call test-name,1,1,1,1,1048576)
+TESTS += $(call test-name,1,1,1,1,2097152)
