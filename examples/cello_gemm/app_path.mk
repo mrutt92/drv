1d4a4c36d94991349d41da201e5cb796ba2405fe
Max Ruttenberg <mrutt@cs.washington.edu>
2024-03-19 12:13:14 -0700

    [cello_gemm] save, correct results

---
+++ examples/cello_gemm/app_path.mk
@@ -0,0 +1,2 @@
+APP_NAME = cello_gemm
+APP_PATH = $(DRV_DIR)/examples/cello_gemm
---
+++ examples/cello_gemm/template.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2024 University of Washington
+
+# import parameters and APP_PATH
+include parameters.mk
+include app_path.mk
+
+DRV_DIR := $(shell git rev-parse --show-toplevel)
+
+include $(DRV_DIR)/mk/cello.mk
+include $(DRV_DIR)/mk/eigen_config.mk
+
+vpath %.c   $(APP_PATH)
+vpath %.cpp $(APP_PATH)
+vpath %.c   $(DRV_DIR)/common/util
+vpath %.cpp $(DRV_DIR)/common/util
+
+APP_EXE ?= $(APP_PATH)/$(test-name)/$(APP_NAME).so
+
+INPUT_DIR := $(DRV_DIR)/inputs/sparse-inputs
+
+SIM_OPTIONS += --core-threads=$(threads) --pod-cores=$(cores)
+SIM_OPTIONS += --pxn-pods=$(pods) --num-pxn=$(pxns)
+SIM_OPTIONS += --core-stats --stats-load-level=3
+
+SIM_THREADS := 1
+include $(DRV_DIR)/mk/config.mk
+include $(DRV_DIR)/mk/application_common.mk
+
+CXXFLAGS += -DGEMM_D0=$(n) -DGEMM_D1=$(m) -DGEMM_D2=$(k)
+CXXFLAGS += $(EIGEN_CXXFLAGS)
+CXXFLAGS += $(CELLO_CXXFLAGS)
+CXXFLAGS += -I$(DRV_DIR)/common/
+CELLO_OBJECTS := $(CELLO_CXXSOURCES_DRVX:.cpp=.o)
+
+$(APP_NAME).so: $(CELLO_OBJECTS)
