5f6079ea9e20753661fbd04a5ffb08c93e5b1135
Max Ruttenberg <mrutt@cs.washington.edu>
2024-03-19 16:35:50 -0700

    [cello_gemm] optimized version working

--- mk/eigen_config.mk
+++ mk/eigen_config.mk
@@ -3,5 +3,5 @@
 ifndef _EIGEN_CONFIG_MK_
 _EIGEN_CONFIG_MK_ = 1
 EIGEN_INSTALL_DIR=$(DRV_ROOT)
-EIGEN_CXXFLAGS += -I$(EIGEN_INSTALL_DIR)/include/eigen3
+EIGEN_CXXFLAGS += -isystem $(EIGEN_INSTALL_DIR)/include/eigen3
 endif
