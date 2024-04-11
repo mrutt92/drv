b480dbbfc094a9e6ed0f819ddd7816dade82fcdb
Max Ruttenberg <mrutt@cs.washington.edu>
2024-04-11 08:54:13 -0700

    change march (imafd=>imaf) and abi (lp64d => lp64)

--- mk/riscv_common.mk
+++ mk/riscv_common.mk
@@ -19,8 +19,8 @@ vpath %.S $(DRV_DIR)/riscv-examples/platform_$(RISCV_PLATFORM)
 vpath %.c $(DRV_DIR)/riscv-examples/platform_$(RISCV_PLATFORM)
 vpath %.cpp $(DRV_DIR)/riscv-examples/platform_$(RISCV_PLATFORM)
 
-RISCV_ARCH:=rv64imafd
-RISCV_ABI:=lp64d
+RISCV_ARCH:=rv64imaf
+RISCV_ABI:=lp64
 
 RISCV_COMPILE_FLAGS += -O2 -march=$(RISCV_ARCH)
 RISCV_COMPILE_FLAGS += -mabi=$(RISCV_ABI)
