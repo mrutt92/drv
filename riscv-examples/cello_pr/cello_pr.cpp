994432144f6eda4d19b4f038e1f3cd2885d67c30
Max Ruttenberg <mrutt@cs.washington.edu>
2024-04-24 14:22:49 -0700

    cello_pr running on drvr

--- riscv-examples/cello_pr/cello_pr.cpp
+++ riscv-examples/cello_pr/cello_pr.cpp
@@ -1,13 +1,52 @@
 #include <iostream>
 #include <stdio.h>
 #include "pandohammer/storage.h"
+#include "pandohammer/mmio.h"
 #include "cello.hpp"
 #include "cello_pr.hpp"
 
+#ifndef ITERATIONS
+#define ITERATIONS 10
+#endif
+
 l2sp_storage(pagerank_config) pagerank_configure;
+l1sp_storage(int32_t) count[CORE_THREADS];
+
+void pagerank(pagerank_config cfg)
+{
+    graph g = *cfg.g();
+    vertex V = g.V();
+    float damp = 0.85f;    
+    float beta_score = (1.0f - damp)/V;
+    pointer<float> contrib = cfg.contrib();
+    pointer<float> old_rank = cfg.old_rank();
+    pointer<float> new_rank = cfg.new_rank();
+    pointer<vertex> out_degree = cfg.out_degree();
+    for (int i = 0; i < ITERATIONS; i++) {
+        cello::parallel_for(0, V, 1, [=](vertex v) mutable {
+            contrib[v] = old_rank[v] / out_degree[v];
+        });
+        cello::parallel_for(0, V, 1, [=](vertex dst) mutable {
+            float rank = 0.0f;
+            vertex start = g.offsets()[dst];
+            vertex end = g.offsets()[dst+1];
+            for (vertex e = start; e < end; e++) {
+                vertex src = g.edges()[e];
+                rank += contrib[src];
+            }
+            new_rank[dst] = damp * new_rank[dst] + beta_score;
+            old_rank[dst] = new_rank[dst];
+        });
+        ph_print_int(-i);
+    }
+}
+
 
 int CelloMain(int argc, char *argv[])
 {
-    printf("graph @ %p\n", pagerank_configure.g());
+    graph g = *pagerank_configure.g();
+    printf("V = %d, E = %d, offsets = %p, edges = %p\n"
+           , g.V(), g.E(), g.offsets(), g.edges());
+    pagerank(pagerank_configure);
     return 0;
 }
--- riscv-examples/cello_pr/cello_pr.hpp
+++ riscv-examples/cello_pr/cello_pr.hpp
@@ -3,10 +3,12 @@
 #include <cstdint>
 #ifndef RISCV
 #include <DrvAPI.hpp>
+#include <vector>
 #endif
 
-typedef int64_t vertex;
+typedef int32_t vertex;
 typedef vertex edge;
+
 #ifdef RISCV
 template <typename T>
 using pointer = T*;
@@ -50,6 +52,16 @@ class value_handle<graph> {
     DRV_API_VALUE_HANDLE_FIELD(graph, E, edge, _E);
     DRV_API_VALUE_HANDLE_FIELD(graph, offsets, pointer<vertex>, _offsets);
     DRV_API_VALUE_HANDLE_FIELD(graph, edges, pointer<edge>, _edges);
+    void init(int V, int E,
+              const std::vector<int> &offsets,const std::vector<int> &edges) {
+        this->V() = (vertex)V;
+        this->E() = (edge)E;
+        constexpr auto memtype = DrvAPI::DrvAPIMemoryDRAM;
+        this->offsets() = (pointer<vertex>)
+            DrvAPI::DrvAPIMemoryAlloc(memtype, (V+1)*sizeof(vertex));
+        this->edges() = (pointer<edge>)
+            DrvAPI::DrvAPIMemoryAlloc(memtype, E*sizeof(edge));
+    }
 };
 }
 #endif
@@ -59,5 +71,34 @@ class value_handle<graph> {
  */
 struct pagerank_config {
     FIELD(pointer<graph>, g, _g); // pointer to the graph
+    FIELD(pointer<float>, old_rank, _old_rank); // pointer to the old rank
+    FIELD(pointer<float>, new_rank, _new_rank); // pointer to the new rank
+    FIELD(pointer<vertex>, out_degree, _out_degree); // pointer to the out degree
+    FIELD(pointer<float>, contrib, _contrib); // pointer to the contribution
+    template <typename Dst, typename Src>
+    static void copy(Dst &dst, const Src &src) {
+        dst.g() = src.g();
+        dst.old_rank() = src.old_rank();
+        dst.new_rank() = src.new_rank();
+        dst.out_degree() = src.out_degree();
+        dst.contrib() = src.contrib();        
+    }
 };
+
+#ifndef RISCV
+namespace DrvAPI
+{
+template <>
+class value_handle<pagerank_config> {
+    DRV_API_VALUE_HANDLE_DEFAULTS(pagerank_config);
+    DRV_API_VALUE_HANDLE_FIELD(pagerank_config, g, pointer<graph>, _g);
+    DRV_API_VALUE_HANDLE_FIELD(pagerank_config, old_rank, pointer<float>, _old_rank);
+    DRV_API_VALUE_HANDLE_FIELD(pagerank_config, new_rank, pointer<float>, _new_rank);
+    DRV_API_VALUE_HANDLE_FIELD(pagerank_config, out_degree, pointer<vertex>, _out_degree);
+    DRV_API_VALUE_HANDLE_FIELD(pagerank_config, contrib, pointer<float>, _contrib);
+};
+}
+#endif
+
+
 #endif
